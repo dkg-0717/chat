243772d26ddf80dd629bcd3ca66a904900e761e1
Angel <angelj@edivers.mx>
2018-08-23 17:12:16 -0500

    Se corrigio env firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDDYwiRY-aVS_r-tr2GejkInpPgnedQYjQ',
+    authDomain: 'firechat-57e62.firebaseapp.com',
+    databaseURL: 'https://firechat-57e62.firebaseio.com',
+    projectId: 'firechat-57e62',
+    storageBucket: 'firechat-57e62.appspot.com',
+    messagingSenderId: '799846893435'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: 'AIzaSyDDYwiRY-aVS_r-tr2GejkInpPgnedQYjQ',
     authDomain: 'firechat-57e62.firebaseapp.com',
